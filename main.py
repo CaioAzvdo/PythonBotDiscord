c8944cc873a6f485268d951b0ea7a13cc1b53774
CaioAzvdo <caioazevedodourado8@gmail.com>
2025-02-24 21:46:52 -0300

    Adicionado comandos de criação de canal de texto e voz

--- main.py
+++ main.py
@@ -24,18 +24,13 @@ class main(discord.Client):
 bot = main()
 
 @bot.tree.command(name="olamundo", description="comando olá mundo")
-async def olaMundo(interaction:discord.Interaction):
+async def ola_mundo(interaction:discord.Interaction):
     await interaction.response.send_message(f"Olá {interaction.user.mention}!")
 
-@bot.tree.command(name="soma", description="comando para somar dois numeros")
-@app_commands.describe(
-    number1 = "Primeiro número a somar",
-    number2 = "Segundo número a somar"
-)
 
 #comando pra atribuir uma role especifica de um servidor especifico, acho que isso é muito especifico
 @bot.tree.command(name="cargofoda", description="comando para se tornar foda")
-async def cargoFoda(interaction:discord.Interaction, user: discord.Member):
+async def cargo_foda(interaction:discord.Interaction, user: discord.Member):
     role = interaction.guild.get_role(1343633150760194098)
     if role:
         await user.add_roles(role)
@@ -49,7 +44,7 @@ async def cargoFoda(interaction:discord.Interaction, user: discord.Member):
 @app_commands.describe(
     role_name="O nome do cargo novo"
 )
-async def criarCargo(interaction:discord.Interaction, user: discord.Member, role_name : str):
+async def criar_cargo(interaction:discord.Interaction, user: discord.Member, role_name : str):
     guild = interaction.guild
     role = await guild.create_role(name=role_name)
     await interaction.response.send_message(f"A role {role.name} foi criada com sucesso! e atribuida a {user.mention}", ephemeral=True)
@@ -64,5 +59,20 @@ async def enviar_embed(interaction:discord.Interaction, user: discord.Member):
 
     await user.send(embed=embed)
     await interaction.response.send_message(f"Embed enviado para {user.mention}", ephemeral=True)
+@bot.tree.command(name="criar_chat_voz", description="crie um chat de voz agora mesmo!")
+async def criar_chat_voz(interaction:discord.Interaction, nome_chat:str):
+    guild = interaction.guild
+    await guild.create_voice_channel(name=f"{nome_chat}")
+    await interaction.response.send_message(f"Canal de voz criado com sucesso!", ephemeral=True)
+
+@bot.tree.command(name="criar_chat_texto", description="crie um chat de texto agora mesmo!")
+async def criar_chat_voz(interaction:discord.Interaction, nome_chat:str):
+    guild = interaction.guild
+    text_channel = await guild.create_text_channel(name=f"{nome_chat}")
+    # channel_id = text_channel.id
+    await text_channel.send("Sou o First! seu panaca")
+
+
+    await interaction.response.send_message(f"Canal de texto criado com sucesso!", ephemeral=True)
 
 bot.run(os.getenv("DISCORD_TOKEN_API"))
